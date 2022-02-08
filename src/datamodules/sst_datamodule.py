0f24c1aeabe91d0125194f169a5ab3e040f7811c
ValeKnappich <valentin.knappich@web.de>
2022-02-08 14:00:12 +0000

    Add sst model and datamodule

---
+++ src/datamodules/sst_datamodule.py
@@ -0,0 +1,51 @@
+import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+import datasets
+from transformers import AutoTokenizer
+
+class SSTDataModule(pl.LightningDataModule):
+
+    def __init__(self, batch_size, model_id):
+        super().__init__()
+        
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+            
+        self.dataset_dict = datasets.load_dataset("sst").map(
+            lambda x: self.tokenizer(
+                x["sentence"], padding="max_length", max_length=65
+            )
+        ).map(
+            # Add or remove epsilone if labels are 1 or 0 to avoid error in BCELoss
+            lambda x: dict(
+                label=x["label"] - 1e-6 if x["label"] == 1 else 
+                x["label"] + 1e-6 if x["label"] == 0 else x["label"], 
+                **{k: v for k, v in x.items() if k != "label"}
+            )
+        )
+        
+        self.dataset_dict.set_format("torch", columns=["input_ids", "attention_mask", "label"])
+        self.batch_size = batch_size
+        
+    
+    def dataloader(self, split):
+        return DataLoader(
+            self.dataset_dict[split], 
+            batch_size=self.batch_size, 
+            num_workers=4, 
+            pin_memory=True, 
+            shuffle=bool(split == "train")
+        )
+    
+    
+    def train_dataloader(self):
+        return self.dataloader("train")
+    
+    
+    def val_dataloader(self):
+        return self.dataloader("validation")
+    
+    
+    def test_dataloader(self):
+        return self.dataloader("test")
+    
