88dc7a97b1032fd6bcc5f54e4c62e05173f192ea
ValeKnappich <valentin.knappich@web.de>
2022-02-10 15:07:13 +0000

    remove unused dir

--- src/accelerator/base.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-import torch
-
-
-class ModelAccelerator(ABC):
-    @abstractmethod
-    def prepare_model() -> torch.Module:
-        """Prepare model for efficient inference, e.g. by quantizing the weights"""
