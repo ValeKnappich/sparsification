5bac77994183d5befec475b7f3bee3023cbcbe7d
ValeKnappich <valentin.knappich@web.de>
2022-02-15 10:37:09 +0000

    update demo

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,9 +1,17 @@
+import pytorch_lightning as pl
 import streamlit as st
 
 
-def build_app():
+def build_app(model: pl.LightningModel):
     st.write("# Hello World")
 
 
 if __name__ == "__main__":
-    build_app()
+    from pathlib import Path
+
+    from src.models import SSTModel
+
+    model = SSTModel.load_from_checkpoint(
+        Path(__file__).parent.parent.parent / "checkpoints/distilroberta-base.ckpt"
+    )
+    build_app(model)
