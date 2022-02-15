45bde593a63a6508aa46bd4dd77ba294a607d85a
ValeKnappich <valentin.knappich@web.de>
2022-02-15 10:40:17 +0000

    update demo

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,9 +1,21 @@
 import pytorch_lightning as pl
 import streamlit as st
+import transformers
 
 
 def build_app(model: pl.LightningModel):
-    st.write("# Hello World")
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model.model_id)
+
+    st.write("# Sentiment Analysis")
+    st.info("The model used here is `distilroberta-base` trained on the `SST` dataset.")
+
+    text_input = st.text_input("Text Input", "This is great")
+
+    with st.spinner(text="Processing text ..."):
+        tokens = tokenizer(text_input, return_tensors="pt")
+        out = model(tokens)
+
+    st.write(out.item())
 
 
 if __name__ == "__main__":
